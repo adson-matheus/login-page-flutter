12140f7c2dd48a25aadc86c6d4086aa44c97dc82
Adson Matheus <adson.matheus62@gmail.com>
2022-09-23 15:09:28 -0300

    feat: rota p/ home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splash_screen/pages/home_page.dart';
 import 'pages/splash_page.dart';
 import 'pages/login_page.dart';
 
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
       routes: {
         "/splash": (_) => const SplashPage(),
         "/login": (_) => LoginPage(),
+        "/home": (_) => const HomePage(),
       },
     );
   }
